a307e52361d95aa3d3e8c66c5dcd3e02553516fb
yarorage <yarorage@users.noreply.github.com>
2026-09-15 00:04:04 +0300

    AutoLoginByYaroRage: v1.3.5, antiafk теперь на прямом вводе в контрол-блок (0xB73458) каждый кадр - работает при свёрнутом окне и не мешает пользоваться ПК

--- moonloader/AutoLoginByYaroRage_v1.3.5.lua
+++ moonloader/AutoLoginByYaroRage_v1.3.5.lua
@@ -2,7 +2,7 @@
 -- Автор: YaroRage
 script_name("AutoLoginByYaroRage")
 script_author("YaroRage")
-script_version("1.3.4")
+script_version("1.3.5")
 
 require 'moonloader'
 local ffi = require('ffi')
